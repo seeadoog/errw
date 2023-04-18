48d1cd5a14641d62bede1889442157c67bf75faf
sjliu7 <1581503100@qq.com>
2023-04-18 15:09:43 +0800

    add caller support

--- error.go
+++ error.go
@@ -1,13 +1,35 @@
 package errw
 
-import "fmt"
+import (
+	"errors"
+	"fmt"
+	"runtime"
+	"strconv"
+	"strings"
+)
 
 type Error interface {
 	error
+	Caller
 	cause
 	Wrap(args ...interface{}) Error
 	Wrapf(f string, args ...interface{}) Error
+	Wraps(s string) Error
 	UnWrap() error
+	WithCaller() Error
+	WithCallers(callerSkip int) Error
+	WithMeta(key string, val any) Error
+	GetMeta(key string) (val any)
+}
+
+type Caller interface {
+	Callers() Callers
+}
+
+type Callers []string
+
+func (c Callers) String() string {
+	return strings.Join([]string(c), "\n")
 }
 
 type cause interface {
@@ -15,16 +37,29 @@ type cause interface {
 }
 
 type werror struct {
-	err error
-	msg string
+	err      error
+	msg      []string
+	stack    Callers
+	metas    map[string]any
+	reversed bool
 }
 
 func (w *werror) Wrap(args ...interface{}) Error {
-	return Wrap(w, args...)
+	w.msg = append(w.msg, fmt.Sprint(args...))
+	return w
+	// return Wrap(w, args...)
 }
 
 func (w *werror) Wrapf(f string, args ...interface{}) Error {
-	return Wrapf(w, f, args...)
+	w.msg = append(w.msg, fmt.Sprintf(f, args...))
+	return w
+	// return Wrapf(w, f, args...)
+
+}
+
+func (w *werror) Wraps(s string) Error {
+	w.msg = append(w.msg, s)
+	return w
 }
 
 func (w *werror) UnWrap() error {
@@ -32,24 +67,98 @@ func (w *werror) UnWrap() error {
 }
 
 func (w *werror) Error() string {
-	return w.msg + " -> " + w.err.Error()
+	if len(w.msg) == 0 {
+		return w.err.Error()
+	}
+	if !w.reversed {
+		reverse(w.msg)
+		w.reversed = true
+	}
+	return strings.Join(append(w.msg, w.err.Error()), " -> ")
 }
 
 func (w *werror) cause() error {
 	return w.err
 }
 
+func (w *werror) WithCallers(callerSkip int) Error {
+	w.stack = append(w.stack, getCall(callerSkip+1))
+	return w
+}
+func (w *werror) WithCaller() Error {
+	return w.WithCallers(1)
+}
+
+func (w *werror) WithMeta(key string, val any) Error {
+	if w.metas == nil {
+		w.metas = map[string]any{}
+	}
+	w.metas[key] = val
+
+	return w
+}
+
+func (w *werror) GetMeta(key string) (val any) {
+	if w.metas != nil {
+		val = w.metas[key]
+		if val != nil {
+			return val
+		}
+	}
+	e, ok := w.err.(*werror)
+	if ok {
+		return e.GetMeta(key)
+	}
+	return nil
+}
+
+func getCall(callSkip int) string {
+	pc, file, line, ok := runtime.Caller(callSkip + 1)
+	if ok {
+		fc := runtime.FuncForPC(pc)
+		funName := ""
+		if fc != nil {
+			funName = fc.Name()
+		}
+		return file + ":" + strconv.Itoa(line) + " " + funName
+	}
+	return ""
+}
+
+func (w *werror) Callers() Callers {
+	c, ok := w.err.(Caller)
+	if ok {
+		return append(c.Callers(), w.stack...)
+	}
+	return w.stack
+}
+
 func Wrap(err error, args ...interface{}) Error {
+
 	return &werror{
 		err: err,
-		msg: fmt.Sprint(args...),
+		msg: []string{fmt.Sprint(args...)},
+	}
+}
+
+func New(s string) Error {
+	return &werror{
+		err: errors.New(s),
+		msg: make([]string, 0, 3),
+	}
+}
+
+func NewWithMsgInitCap(s string, cap int) Error {
+	return &werror{
+		err: errors.New(s),
+		msg: make([]string, 0, cap),
 	}
 }
 
 func Wrapf(err error, f string, args ...interface{}) Error {
 	return &werror{
 		err: err,
-		msg: fmt.Sprintf(f, args...),
+		msg: []string{fmt.Sprintf(f, args...)},
 	}
 }
 
@@ -63,5 +172,24 @@ func Unwrap(err error) error {
 			continue
 		}
 		return err
+	}
+}
+
+func CallersOf(err error) Callers {
+	e, ok := err.(Caller)
+	if ok {
+		return e.Callers()
+	}
+	return nil
+}
+
+func reverse(s []string) {
+	i := 0
+	j := len(s) - 1
+
+	for i < j {
+		s[i], s[j] = s[j], s[i]
+		i++
+		j--
 	}
 }
--- error_test.go
+++ error_test.go
@@ -1,29 +1,62 @@
 package errw
 
 import (
-	"errors"
 	"fmt"
 	"testing"
 )
 
 type error2 = Error
 
-func newRequest() error {
-	return errors.New("new request error")
+func newRequest() error2 {
+	return NewWithMsgInitCap("new request error", 5)
 }
 
 func get() error2 {
 	err := newRequest()
-	return Wrap(err, "get")
+	return err.Wraps("new request error")
 }
 
 func doIt() error2 {
 	err := get()
-	return err.Wrap("doIt")
+	return err.Wraps("doit: get error").Wraps("ggg")
 }
 
 func TestError(t *testing.T) {
 	err := doIt()
-	fmt.Println(err)
-	fmt.Println(err.UnWrap())
+	re := err.UnWrap()
+	fmt.Println(err.Error(), "caller:\n", err.Callers(), "info", "raw error", re)
+	fmt.Println(err.Error(), "caller:\n", err.Callers(), "info", "raw error", re)
+	fmt.Println(err.GetMeta("code"), err.GetMeta("message"))
+}
+
+func stackOf() {
+	defer func() {
+		recover()
+
+	}()
+
+	panic("gg")
+}
+
+func BenchmarkName(b *testing.B) {
+	// TODO: Initialize
+	for i := 0; i < b.N; i++ {
+		// TODO: Your Code Here
+		// stackOf()
+		doIt().Error()
+
+	}
+}
+
+func BenchmarkPa(b *testing.B) {
+	// TODO: Initialize
+	e := 1
+	for i := 0; i < b.N; i++ {
+		assert(e)
+
+	}
+}
+
+func assert(v any) {
+	// _, _ = v.(Error)
 }
